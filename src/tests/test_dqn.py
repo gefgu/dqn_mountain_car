d6119535751848e3ff50a28457b98f34f97685e9
Gustavo <gefgu12@gmail.com>
2020-12-21 09:39:36 -0300

    Addition: Test of dqn output shape

---
+++ src/tests/test_dqn.py
@@ -0,0 +1,8 @@
+from ..dqn import DQN
+from .. import config
+from torch import rand
+
+def test_dqn_output_shape():
+    dqn = DQN()
+    out = dqn(rand(config.N_STATE_FEATURES))
+    assert out.shape == (config.N_ACTIONS,)
