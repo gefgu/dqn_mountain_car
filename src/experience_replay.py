347ac2fb0bc867b2aa47f899ed2cf0c11362ced3
Gustavo <gefgu12@gmail.com>
2020-12-21 08:59:39 -0300

    Addition: Experience Replay test

--- src/experience_replay.py
+++ src/experience_replay.py
@@ -1,5 +1,6 @@
 from collections import deque 
 from random import sample
+from numpy import array
 
 class ExperienceReplay:
     def __init__(self, maxlen=1000000):
@@ -10,4 +11,6 @@ class ExperienceReplay:
 
     def sample_minibatch(self, bs=32):
         minibatch = sample(self.memory, k=bs)
-        return minibatch
+        minibatch = array(minibatch)
+        return minibatch[:, 0], minibatch[:, 1], minibatch[:, 2], \
+                minibatch[:, 3], minibatch[:, 4]
