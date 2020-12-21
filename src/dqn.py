1b391744f6534f18c553bb17f2121d829de545d5
Gustavo <gefgu12@gmail.com>
2020-12-21 09:22:35 -0300

    Addition: DQN and config

---
+++ src/dqn.py
@@ -0,0 +1,16 @@
+from torch import nn
+import torch.nn.functional as F
+from torch.fun
+from . import config
+
+class DQN(nn.Module):
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.linear1 = nn.Linear(config.STATE_FEATURES, 32)
+        self.linear2 = nn.Linear(32, 64)
+        self.linear3 = nn.Linear(64, config.N_ACTIONS)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        return self.linear3(x)
