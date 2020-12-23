773cfca264fb6f86f99eb5c3d232ebdfd8553a87
Gustavo <gefgu12@gmail.com>
2020-12-23 10:42:09 -0300

    Fixed outdated test

--- src/tests/test_experience_replay.py
+++ src/tests/test_experience_replay.py
@@ -1,13 +1,13 @@
-from src import ExperienceReplay
+from src import ExperienceReplay, experience_replay
 from numpy import array_equal
 
 
 def test_sample_minibatch():
-    experience_replay = ExperienceReplay(maxlen=3)
+    experience_replay = ExperienceReplay()
     for i in range(2):
-        experience_replay.store_transition(1, 2, 3, 4)
-    states, actions, rewards, next_states = experience_replay.sample_minibatch(bs=2)
-    assert array_equal(states, [1, 1])
-    assert array_equal(actions, [2, 2])
-    assert array_equal(rewards, [3, 3])
-    assert array_equal(next_states, [4, 4])
+        experience_replay.append(([1, 1], 2, 3, [4, 4]))
+    minibatch = experience_replay.sample(size=2)
+    assert [elem[0] for elem in minibatch] == [[1, 1], [1, 1]]
+    assert [elem[1] for elem in minibatch] == [2, 2]
+    assert [elem[2] for elem in minibatch] == [3, 3]
+    assert [elem[3] for elem in minibatch] == [[4, 4], [4, 4]]
